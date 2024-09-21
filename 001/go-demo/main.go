f2dd99ae819ed638db93464d9ab4fb4ff84286cf
Max Basev <64890011+MaxBasev@users.noreply.github.com>
2024-09-21 21:15:13 +0600

    Update main.go

--- 001/go-demo/main.go
+++ 001/go-demo/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Printf("Hello World!!!!")
+	var userHeight = 1.8
+	var userWeight float64 = 60
+	var BMI = userWeight / math.Pow(userHeight, 2)
+
+	fmt.Println("Body mass index calculation.")
+	fmt.Print(BMI)
 }
