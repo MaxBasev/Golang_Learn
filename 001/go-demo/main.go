710f29330351eb72c8a14ac93dea6806b373b844
Max Basev <64890011+MaxBasev@users.noreply.github.com>
2024-09-21 21:23:18 +0600

    Update main.go
    try const

--- 001/go-demo/main.go
+++ 001/go-demo/main.go
@@ -6,9 +6,10 @@ import (
 )
 
 func main() {
+	const BMIPower = 2
 	userHeight := 1.8
 	userWeight := 60.0
-	BMI := userWeight / math.Pow(userHeight, 2)
+	BMI := userWeight / math.Pow(userHeight, BMIPower)
 
 	fmt.Println("Body mass index calculation.")
 	fmt.Print(BMI)
