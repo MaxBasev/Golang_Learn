2078e7f19905b8cf0df2752d0b0d036e4aae4c36
Max Basev <64890011+MaxBasev@users.noreply.github.com>
2024-10-01 15:20:16 +0600

    Update main.go

--- 001/go-demo-2/main.go
+++ 001/go-demo-2/main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	transactions := [3]int{1, 5, 8}
+	banks := [3]string{"AmeX", "Revolut"}
+
+	fmt.Println(transactions[0])
+	banks[2] = "Aktif"
+	fmt.Println(banks)
 }
