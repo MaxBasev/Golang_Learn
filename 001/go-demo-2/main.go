5e20c4902401a46d473b881771be0773e450f5f3
Max Basev <64890011+MaxBasev@users.noreply.github.com>
2024-10-03 18:16:40 +0600

    Update main.go

--- 001/go-demo-2/main.go
+++ 001/go-demo-2/main.go
@@ -4,11 +4,13 @@ import "fmt"
 
 func main() {
 	transactions := [5]int{1, 2, 3, 4, 5}
+	transactionsNew := transactions
 	banks := [2]string{}
 
-	fmt.Println(transactions[0])
 	banks[0] = "Aktif"
 	fmt.Println(banks)
 	partial := transactions[1:4]
 	fmt.Println(partial)
+	fmt.Println(transactions)
+	fmt.Println(transactionsNew)
 }
